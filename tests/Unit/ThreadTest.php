d518e923510bd81ba39d4b5aada52beae09d31ed
shield02 <myshield02@gmail.com>
2019-06-18 03:19:54 +0100

    initial commit all files

---
+++ tests/Unit/ThreadTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ThreadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function a_thread_has_replies()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
+    }
+
+    /** @test */
+    function a_thread_has_a_creator() 
+    {
+        $thread = factory('App\Thread')->create();
+
+        $this->assertInstanceOf('App\User', $thread->creator);
+    }
+}
