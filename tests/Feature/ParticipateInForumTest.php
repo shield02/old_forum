5532d8d80742e971a0154d8413caab69c58d0ce7
shield02 <myshield02@gmail.com>
2019-06-19 02:44:59 +0100

    User may respond to threads

---
+++ tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ParticipateInForumTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->post('/threads/1/replies', []);
+        
+    }
+    
+    /** @test */
+    function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        //Given we have an authenticated user
+        $this->be($user = factory('App\User')->create());
+
+        //And an existing thread
+        $thread = factory('App\Thread')->create();
+
+        //When a user adds a reply to the thread
+        $reply = factory('App\Reply')->make();
+        $this->post($thread->path(). '/replies', $reply->toArray());
+
+        //Then their reply should be visible on the page
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+}
