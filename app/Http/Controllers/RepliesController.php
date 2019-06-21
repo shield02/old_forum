16590985696503dc02f247a101624d4b5b54ca59
shield02 <myshield02@gmail.com>
2019-06-21 06:50:54 +0100

    user may publish threads

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
+    /**
+     * RepliesController Constructor
+     */
     public function __construct()
     {
         $this->middleware('auth');
