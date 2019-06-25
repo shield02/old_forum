91a831b7a5439079ff0e28a801fc24ce74a62d3d
shield02 <myshield02@gmail.com>
2019-06-25 06:42:00 +0100

    persist a new reply with validation

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -15,8 +15,19 @@ class RepliesController extends Controller
         $this->middleware('auth');
     }
 
-    public function store(Thread $thread)
+    /**
+     * Persist a new reply.
+     *
+     * @param $channelId
+     * @param  \App\Thread  $thread
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store($channelId, Thread $thread)
     {
+        $this->validate(request(), [
+            'body' => 'required'
+        ]);
+
         $thread->addReply([
             'body'      => request('body'),
             'user_id'   => auth()->id()
