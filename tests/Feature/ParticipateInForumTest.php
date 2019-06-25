9562c20f3efaf514110cf158ca1840892d71a0d2
shield02 <myshield02@gmail.com>
2019-06-25 06:52:22 +0100

    test unathenticated user may not add replies with exception handling activated

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -12,9 +12,9 @@ class ParticipateInForumTest extends TestCase
     /** @test */
     function unauthenticated_users_may_not_add_replies()
     {
-        $this->expectException('Illuminate\Auth\AuthenticationException');
-
-        $this->post('/threads/1/replies', []);
+        $this->withExceptionHandling()
+            ->post('/threads/some-channel/1/replies', [])
+            ->assertRedirect('login');
         
     }
     
@@ -22,17 +22,30 @@ class ParticipateInForumTest extends TestCase
     function an_authenticated_user_may_participate_in_forum_threads()
     {
         //Given we have an authenticated user
-        $this->be($user = factory('App\User')->create());
+        $this->signIn();
 
         //And an existing thread
-        $thread = factory('App\Thread')->create();
+        $thread = create('App\Thread');
 
         //When a user adds a reply to the thread
-        $reply = factory('App\Reply')->make();
+        $reply = make('App\Reply');
         $this->post($thread->path(). '/replies', $reply->toArray());
 
         //Then their reply should be visible on the page
         $this->get($thread->path())
             ->assertSee($reply->body);
+    }
+
+    /** @test */
+    function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path(). '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+
     }
 }
