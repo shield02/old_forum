6ed51423393348313b497cde66d5caaa464d9fbd
shield02 <myshield02@gmail.com>
2019-06-28 21:43:00 +0100

    added a query scope method that applies a query to filter threads

--- app/Thread.php
+++ app/Thread.php
@@ -65,4 +65,9 @@ class Thread extends Model
     {
         $this->replies()->create($reply);
     }
+
+    public function scopeFilter($query, $filters)
+    {
+        return $filters->apply($query);
+    }
 }
