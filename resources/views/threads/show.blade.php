0c61c5f1e16d844a2fc34ec7ce1a7efc4a883dfa
shield02 <myshield02@gmail.com>
2019-07-01 22:14:18 +0100

    minor code formatting

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -41,8 +41,8 @@
             <div class="card mb-4">
                 <div class="card-body">
                     <p>
-                        This thread was published {{ $thread->created_at->diffForHumans() }}
-                        by <a href="#">{{ $thread->creator->name }}</a>, 
+                        This thread was published {{ $thread->created_at->diffForHumans() }} by 
+                        <a href="#">{{ $thread->creator->name }}</a>, 
                         and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
                     </p>
                 </div>
