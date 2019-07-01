5052618c28078f8c65daf51805871d97d17d8147
shield02 <myshield02@gmail.com>
2019-07-01 22:12:02 +0100

    can filter threads by popularity

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -67,4 +67,24 @@ class ReadThreadsTest extends TestCase
             ->assertSee($threadByJohn->title)
             ->assertDontSee($threadNotByJohn->title);
     }
+    
+    /** @test */
+    function a_user_can_filter_threads_by_popularity()
+    {
+        // Given we have three threads
+        // With 2 replies, 3 replies, and 0 replies respectively.
+        $threadWithTwoReplies = create('App\Thread');
+        create('App\Reply', ['thread_id' => $threadWithTwoReplies->id], 2);
+        
+        $threadWithThreeReplies = create('App\Thread');
+        create('App\Reply', ['thread_id' => $threadWithThreeReplies->id], 3);
+
+        $threadWithNoReplies = $this->thread;
+
+        // When i filter all threads by popularity
+        $response = $this->getJson('threads?popular=1')->json();
+
+        // Then they should be returned from most to least.
+        $this->assertEquals([3, 2, 0], array_column($response, 'replies_count'));
+    }
 }
