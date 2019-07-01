4a894a70c172a1ad06fc3e285608674714abdf97
shield02 <myshield02@gmail.com>
2019-07-01 22:15:20 +0100

    added popularity dropdown and some styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <style>
+        body { padding-bottom: 100px; }
+        .level { display: flex; align-items: center; }
+        .flex { flex: 1; }
+    </style>
 </head>
-<body style="padding-bottom: 100px;">
+<body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -43,6 +49,8 @@
                                 @if (auth()->check())
                                     <li><a href="/threads?by={{ auth()->user()->name }}" class="dropdown-item">{{ __('My Threads') }}</a></li>
                                 @endif
+
+                                <li><a href="/threads?popular=1" class="dropdown-item">Popular Threads</a></li>
                             </ul>
                         </li>
 
