67ca230a14bfa18eb935a2f907c22519b4fe5886
shield02 <myshield02@gmail.com>
2019-07-04 19:51:55 +0100

    minor UI code optimization

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,21 @@
 <div class="card mb-4">
     <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a> said {{ $reply->created_at->diffForHumans() }} ...
+        <div class="level">
+            <h5 class="flex">
+                <a href="#" class="flex">
+                    {{ $reply->owner->name }}
+                </a> said {{ $reply->created_at->diffForHumans() }} ...
+            </h5>
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    {{ csrf_field() }}
+
+                    <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
     
     <div class="card-body">
