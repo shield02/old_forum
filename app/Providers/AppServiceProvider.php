da7c123c71f361439f2d437a93f0f26b68d77861
shield02 <myshield02@gmail.com>
2019-07-04 19:54:44 +0100

    regitering of debug bar and optimizing the channels query with cache

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
     }
 
     /**
@@ -29,7 +31,11 @@ class AppServiceProvider extends ServiceProvider
         // \View::share('channels', \App\Channel::all());
         
         \View::composer('*', function ($view) {
-            $view->with('channels', \App\Channel::all());
+            $channels = \Cache::rememberForever('channels', function () {
+                return \App\Channel::all();
+            });
+            
+            $view->with('channels', $channels);
         });
 
     }
