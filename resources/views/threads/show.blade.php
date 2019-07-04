91a5552b53c13d1232b1706a8d865ecd4e8a59c3
shield02 <myshield02@gmail.com>
2019-07-04 22:04:11 +0100

    route for users added

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -42,7 +42,7 @@
                 <div class="card-body">
                     <p>
                         This thread was published {{ $thread->created_at->diffForHumans() }} by 
-                        <a href="#">{{ $thread->creator->name }}</a>, 
+                        <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>, 
                         and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
                     </p>
                 </div>
