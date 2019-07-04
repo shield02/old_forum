9826480b5355aa45264e0757e4aa837381396bcd
shield02 <myshield02@gmail.com>
2019-07-04 20:07:44 +0100

    earger load relationship queries and use favoritable trait

--- app/Reply.php
+++ app/Reply.php
@@ -3,11 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Favoritable;
 
 class Reply extends Model
 {
+    use Favoritable;
+
+    /**
+     * Don't auto-apply mass assignment protection.
+     * 
+     * @var array
+     */
     protected $guarded = [];
+
+    /**
+     * Earger load this relationship with owner.
+     * 
+     * 
+     */
+    protected $with = ['owner', 'favorites'];
     
+    /**
+     * A reply has an owner.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
