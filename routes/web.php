f5148c77ea4762d232d5c758a1fc5ca6250a32a7
shield02 <myshield02@gmail.com>
2019-07-04 21:43:04 +0100

    users profile route

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,6 @@ Route::get('threads/{channel}', 'ThreadsController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 Route::post('replies/{reply}/favorites', 'FavoritesController@store');
 
+Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
+
 // Route::resource('threads', 'ThreadsController');
