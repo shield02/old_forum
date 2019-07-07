fe16c153f5f0702e90bf813ed769ae3c8ad437a4
shield02 <myshield02@gmail.com>
2019-07-07 20:05:31 +0100

    change of foreach to forelse and empty declerative

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,29 +4,29 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Forum Threads</div>
-
-                <div class="card-body">
-                    @foreach ($threads as $thread)
-                        <article>
-                            <div class="level">
-                                <h4 class="flex">
-                                    <a href="{{ $thread->path() }}">
-                                        {{ $thread->title }}
-                                    </a>
-                                </h4>
+            @forelse ($threads as $thread)
+                <div class="card">
+                    <div class="card-header">
+                        <div class="level">
+                            <h4 class="flex">
                                 <a href="{{ $thread->path() }}">
-                                    {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}
+                                    {{ $thread->title }}
                                 </a>
-                            </div>
-                            <div class="body">{{ $thread->body }}</div>
-                        </article>
-                        
-                        <hr>
-                    @endforeach
+                            </h4>
+
+                            <a href="{{ $thread->path() }}">
+                                {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="body">{{ $thread->body }}</div>
+                    </div>
                 </div>
-            </div>
+            @empty
+                <p>There are no relevant results at this time.</p>
+            @endforelse
         </div>
     </div>
 </div>
