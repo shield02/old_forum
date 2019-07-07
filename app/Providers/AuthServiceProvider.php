fbcbdf5253d1bb713b545a3e21c5281d0476a406
shield02 <myshield02@gmail.com>
2019-07-07 20:25:23 +0100

    adding a user as an admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function ($user) {
+            if ($user->name == 'JohnDoe') return true;
+        });
     }
 }
