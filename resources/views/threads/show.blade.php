74a981e9c172a947cbf1c6becaf8ea06a04f04f6
shield02 <myshield02@gmail.com>
2019-07-07 20:07:49 +0100

    authorization declerative to restrict delete icon

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,8 +6,21 @@
         <div class="col-md-8">
             <div class="card mb-4">
                 <div class="card-header">
-                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                    {{ $thread->title }}
+                    <div class="level">
+                        <span class="flex">
+                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
+                            {{ $thread->title }}
+                        </span>
+
+                        @can ('update', $thread)
+                            <form action="{{ $thread->path() }}" method="POST">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+
+                                <button type="submit" class="btn btn-link">Delete Thread</button>
+                            </form>
+                        @endcan
+                    </div>
                 </div>
 
                 <div class="card-body">
