642500c97d00bcdac5524c842b73ac719b2e3211
shield02 <myshield02@gmail.com>
2019-07-07 20:27:31 +0100

    authenticated and unauthenticated user delete thread test

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -70,7 +70,36 @@ class CreateThreadsTest extends TestCase
             ->assertSessionHasErrors('channel_id');
 
     }
+
+    /** @test */
+    function unauthorized_users_may_not_delete_threads()
+    {
+        $this->withExceptionHandling();
+
+        $thread = create('App\Thread');
+
+        $this->delete($thread->path())->assertRedirect('/login');
         
+        $this->signIn();
+        $this->delete($thread->path())->assertStatus(403);
+    }
+
+    /** @test */
+    function authorized_users_can_delete_threads()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread', ['user_id' => auth()->id()]);
+        $reply = create('App\Reply', ['thread_id' => $thread->id]);
+
+        $response = $this->json('DELETE', $thread->path());
+
+        $response->assertStatus(204);
+
+        $this->assertDatabaseMissing('threads', ['id'  => $thread->id]);
+        $this->assertDatabaseMissing('replies', ['id'  => $reply->id]);
+    }
+  
     public function publishThread($overrides = [])
     {
         $this->withExceptionHandling()->signIn();
