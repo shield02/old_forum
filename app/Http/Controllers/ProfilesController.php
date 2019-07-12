76b5444c7a03a468ae720376386eb7ba54e78032
shield02 <myshield02@gmail.com>
2019-07-12 21:26:35 +0100

    querying a user activity feed

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Activity;
 
 class ProfilesController extends Controller
 {
+    /**
+     * Show the user's profile.
+     * 
+     * @param App\User $user
+     * @return /Response
+     */
     public function show(User $user)
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'threads' => $user->threads()->paginate(30)
+            'activities' => Activity::feed($user)
         ]);
     }
 }
