ce0e8a10407f72fc482d12f94a7408ff9cc5431f
shield02 <myshield02@gmail.com>
2019-07-12 21:29:33 +0100

    a method to retun activity feed for a user

--- app/Activity.php
+++ app/Activity.php
@@ -6,10 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
+    protected $take;
+
+    /**
+     * Don't auto-apply any mass assignment protection.
+     * 
+     * @var array $guarded
+     */
     protected $guarded = [];
 
+    /**
+     * Fetch the associated subject for the activity.
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\MorphTo
+     */
     public function subject()
     {
         return $this->morphTo();
+    }
+
+    /**
+     * 
+     * @param mixed $user
+     * @param int $take
+     * @return mixed
+     */
+    public static function feed($user, $take = 50) 
+    {
+        return static::where('user_id', $user->id)
+        ->latest()
+        ->with('subject')
+        ->take($take)
+        ->get()
+        ->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
     }
 }
