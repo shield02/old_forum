ac9c283ddf42a5d247b73c350b030a9ed77b07f4
shield02 <myshield02@gmail.com>
2019-07-16 21:29:00 +0100

    added polymorphic relationship

--- app/Favorite.php
+++ app/Favorite.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Favorite extends Model
 {
+    use RecordsActivity;
+
+    /**
+     * Don't auto apply mass assignment protection.
+     * 
+     * @var array $guarded
+     */
     protected $guarded = [];
+
+    public function favorited()
+    {
+        return $this->MorphTo();
+    }
 }
