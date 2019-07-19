3ad28643861a5ec573e902a10235230d42bfd8ff
shield02 <myshield02@gmail.com>
2019-07-19 19:36:51 +0100

    modified to a vue inline template component

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,24 +1,42 @@
-<div id="reply-{{ $reply->id }}" class="card mb-4">
-    <div class="card-header">
-        <div class="level">
-            <h5 class="flex">
-                <a href=" {{ route('profile', $reply->owner) }}" class="flex">
-                    {{ $reply->owner->name }}
-                </a> said {{ $reply->created_at->diffForHumans() }} ...
-            </h5>
-            <div>
-                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
-                    {{ csrf_field() }}
+<reply-component :attributes="{{ $reply }}" inline-template v-cloak>
+    <div id="reply-{{ $reply->id }}" class="card mb-4">
+        <div class="card-header">
+            <div class="level">
+                <h5 class="flex">
+                    <a href=" {{ route('profile', $reply->owner) }}" class="flex">
+                        {{ $reply->owner->name }}
+                    </a> said {{ $reply->created_at->diffForHumans() }} ...
+                </h5>
+                <div>
+                    <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                        {{ csrf_field() }}
 
-                    <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                        {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}
-                    </button>
-                </form>
+                        <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                            {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
+        
+        <div class="card-body">
+            <div v-if="editing">
+                <div class="form-group">
+                    <textarea class="form-control" v-model="body">{{ $reply->body }}</textarea>
+                </div>
+
+                <button class="btn btn-xs btn-primary" @click="update">Update</button>
+                <button class="btn btn-xs btn-link" @click="editing = false">Cancel</button>
+            </div>
+
+            <div v-else v-text="body"></div>
+        </div>
+
+        @can ('update', $reply)
+            <div class="card-footer level">
+                <button class="btn btn-xs mr-1" @click="editing = true">Edit</button>
+                <button class="btn btn-xs btn-danger mr-1" @click="destroy">Delete</button>
+            </div>
+        @endcan
     </div>
-    
-    <div class="card-body">
-        {{ $reply->body }}
-    </div>
-</div>
+</reply-component>
