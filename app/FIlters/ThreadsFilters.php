000bd1c275528397533890cede9bd80d5afa3f2d
shield02 <myshield02@gmail.com>
2019-07-24 15:54:30 +0100

    added filters to filter query according to unanswered threads

--- app/FIlters/ThreadsFilters.php
+++ app/FIlters/ThreadsFilters.php
@@ -6,7 +6,7 @@ use App\User;
 
 class ThreadsFilters extends Filters
 {   
-    protected $filters = ['by', 'popular'];
+    protected $filters = ['by', 'popular', 'unanswered'];
 
     /**
      * Filter the query by a given username.
@@ -32,5 +32,15 @@ class ThreadsFilters extends Filters
         $this->builder->getQuery()->orders = [];
 
         return $this->builder->orderBy('replies_count', 'desc');
+    }
+
+    /**
+     * Filter the query according to unanswered threads.
+     * 
+     * @return mixed
+     */
+    protected function unanswered() 
+    {
+        return $this->builder->where('replies_count', 0);
     }
 }
