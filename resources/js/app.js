020c35e287ae649c501aaacd5dc25f96feee9845
shield02 <myshield02@gmail.com>
2019-07-24 12:10:56 +0100

    globaly load the thread view component

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,8 @@ window.Vue = require('vue');
 
 Vue.component('flash-component', require('./components/FlashComponent.vue').default);
 
+Vue.component('thread-view-component', require('./pages/ThreadComponent.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
