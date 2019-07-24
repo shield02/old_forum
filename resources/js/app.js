4cd0fbfe802933e005b7126a2394fa50663dd759
shield02 <myshield02@gmail.com>
2019-07-24 16:05:18 +0100

    globally load paginator component

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('flash-component', require('./components/FlashComponent.vue').default);
+Vue.component('paginator-component', require('./components/PaginatorComponent.vue').default);
 
 Vue.component('thread-view-component', require('./pages/ThreadComponent.vue').default);
 
