f7cf3f579dc826abd8237e0080a3bf58b037c1ac
shield02 <myshield02@gmail.com>
2019-07-26 02:20:48 +0100

    test a user can subscribe to threads

---
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SubscribeToThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function a_user_can_subscribe_to_threads()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $this->post($thread->path() . '/subscriptions');
+
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply here.'
+        ]);
+
+        // $this->assertCount(1, auth()->user()->notifications);
+    }
+}
