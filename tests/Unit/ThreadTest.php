504b44f22c1bab0b53499aa53ab93ac529924042
shield02 <myshield02@gmail.com>
2019-07-26 02:16:14 +0100

    model test that a user can subscribe, unsubscribe, and have subscriptions for threads

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -49,7 +49,6 @@ class ThreadTest extends TestCase
 
     }
 
-
     /** @test */
     function a_thread_can_add_a_reply()
     {
@@ -67,5 +66,30 @@ class ThreadTest extends TestCase
         $thread = create('App\Thread');
 
         $this->assertInstanceOf('App\Channel', $thread->channel);
+    }
+
+    /** @test */
+    function a_thread_can_be_subscribed_to()
+    {
+        $thread = create('App\Thread');
+
+        $thread->subscribe($userId = 1);
+
+        $this->assertEquals(
+            1, 
+            $thread->subscriptions()->where('user_id', $userId)->count()
+        );
+    }
+
+    /** @test */
+    function a_thread_can_be_unsubscribed_from()
+    {
+        $thread = create('App\Thread');
+
+        $thread->subscribe($userId = 1);
+
+        $thread->unsubscribe($userId);
+
+        $this->assertCount(0, $thread->subscriptions);
     }
 }
