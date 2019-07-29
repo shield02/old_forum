b49e57dc55502c0a6982a383fc29fa2e4b913b8b
shield02 <myshield02@gmail.com>
2019-07-29 10:00:57 +0100

    vue component for subcribe button

---
+++ resources/js/components/SubscribeButtonComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <button :class="classes" @click="subscribe">Subcribe</button>
+</template>
+
+<script>
+    export default {
+        props: ['active'],
+
+        computed: {
+            classes() {
+                return [
+                    'btn', 
+                    this.active ? 'btn-primary' : 'btn-light'
+                ];
+            }
+        },
+
+        methods: {
+            subscribe() {
+                axios.post(location.pathname + '/subscriptions');
+
+                this.active = true;
+
+                flash('Subscribed');
+            }
+        }
+    }
+</script>
