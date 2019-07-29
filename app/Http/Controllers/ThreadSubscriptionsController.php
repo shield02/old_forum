7a77182eaaa171164e207ffb3cf183ec14008a9e
shield02 <myshield02@gmail.com>
2019-07-29 10:26:46 +0100

    method to delete a thread subscription

--- app/Http/Controllers/ThreadSubscriptionsController.php
+++ app/Http/Controllers/ThreadSubscriptionsController.php
@@ -7,11 +7,23 @@ use Illuminate\Http\Request;
 
 class ThreadSubscriptionsController extends Controller
 {
+    /**
+     * 
+     * 
+     * @param mixed $channelId
+     * @param App\Thread $thread
+     */
     public function store($channelId, Thread $thread)
     {
         $thread->subscribe();
     }
 
+    /**
+     * 
+     * 
+     * @param mixed $channelId
+     * @param App\Thread $thread
+     */
     public function destroy($channelId, Thread $thread)
     {
         $thread->unsubscribe();
