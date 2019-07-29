e8b6a92039e28d1a510324434d9877841a130d17
shield02 <myshield02@gmail.com>
2019-07-29 09:56:44 +0100

    import the subscribe button component

--- resources/js/pages/ThreadComponent.vue
+++ resources/js/pages/ThreadComponent.vue
@@ -1,10 +1,11 @@
 <script>
     import RepliesComponent from '../components/RepliesComponent.vue';
+    import SubscribeButtonComponent from '../components/SubscribeButtonComponent.vue';
 
     export default {
         props: ['initialRepliesCount'],
 
-        components: { RepliesComponent },
+        components: { RepliesComponent, SubscribeButtonComponent },
 
         data() {
             return {
