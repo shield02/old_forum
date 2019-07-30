7adeda80206ec725fc1f180b4f98403c237420af
shield02 <myshield02@gmail.com>
2019-07-30 21:01:05 +0100

    subscription relationships

--- app/ThreadSubscription.php
+++ app/ThreadSubscription.php
@@ -3,8 +3,44 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Notifications\ThreadWasUpdated;
 
 class ThreadSubscription extends Model
 {
+    /**
+     * The attributes that aren't mass assignable.
+     * 
+     * @var array
+     */
     protected $guarded = [];
+
+    /**
+     * A subscription belongs to a user.
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * A subscription is associated to a thread.
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
+
+    /**
+     * 
+     * 
+     * @param mixed $reply
+     */
+    public function notify($reply)
+    {
+        $this->user->notify(new ThreadWasUpdated($this->thread, $reply));
+    }
 }
