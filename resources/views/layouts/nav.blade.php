6579ba6378646dfe93959f0a8a562e2d1629550e
shield02 <myshield02@gmail.com>
2019-07-31 01:49:39 +0100

    added a notifications component

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -56,6 +56,8 @@
                         </li>
                     @endif
                 @else
+                    <user-notifications-component></user-notifications-component>
+                    
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
