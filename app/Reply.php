6e0a141daf54ba70acbf187f73c903ae4253c78e
shield02 <myshield02@gmail.com>
2019-08-17 20:09:52 +0100

    custom setter for body attributes

--- app/Reply.php
+++ app/Reply.php
@@ -76,13 +76,13 @@ class Reply extends Model
     }
 
     /**
-     * 
+     * Fetch all mentioned users within the reply's body.
      * 
      * @return string[]
      */
     public function mentionedUsers()
     {
-        preg_match_all('/\@([^\s\.]+)/', $this->body, $matches);
+        preg_match_all('/@([\w\-]+)/', $this->body, $matches);
 
         return $matches[1];
 
@@ -96,5 +96,15 @@ class Reply extends Model
     public function path()
     {
         return $this->thread->path() . "#reply-{$this->id}";
+    }
+
+    /**
+     * 
+     * 
+     * @param mixed $body
+     */
+    public function setBodyAttribute($body)
+    {
+        $this->attributes['body'] = preg_replace('/@([\w\-]+)/', '<a href="/profiles/$1">$0</a>', $body);
     }
 }
