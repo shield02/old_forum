9de1982d56b5ea1be115b1651006b0dd9e7576ef
shield02 <myshield02@gmail.com>
2019-08-19 04:09:04 +0100

    minor changes

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -12,6 +12,8 @@
                     <div class="card mb-4">
                         <div class="card-header">
                             <div class="level">
+                                <img src="{{ $thread->creator->avatar_path }}" alt="{{ $thread->creator->name }}" width="25" height="25" class="mr-2">
+
                                 <span class="flex">
                                     <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
                                     {{ $thread->title }}
