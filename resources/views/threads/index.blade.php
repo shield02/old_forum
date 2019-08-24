4ce620104f5cf3bc942e8bbd41382d1d5a9de4eb
shield02 <myshield02@gmail.com>
2019-08-24 04:33:03 +0100

    add a trending threads widget

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -2,11 +2,28 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-md-8">
             @include('threads._list')
 
             {{ $threads->render() }}
+        </div>
+
+        <div class="col-md-4">
+            <div class="card card-default">
+                <div class="card-header">
+                    Trending threads
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach ($trending as $thread)
+                                <li class="list-group-item">
+                                    <a href="{{ url($thread->path) }}">{{ $thread->title }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
