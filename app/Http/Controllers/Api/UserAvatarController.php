1f90ff8073c1718e04c08fa5332d742efe4fdd54
shield02 <myshield02@gmail.com>
2019-08-24 04:30:18 +0100

    store a new avatar by a user

---
+++ app/Http/Controllers/Api/UserAvatarController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserAvatarController extends Controller
+{
+    /**
+     * Persit a new avatar.
+     * 
+     * @param Illuminate\Http\Request $request
+     * @return Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'avatar' => ['required', 'image']
+        ]);
+
+        auth()->user()->update([
+            'avatar_path' => request()->file('avatar')->store('avatars', 'storage')
+        ]);
+
+        return response([], 204);
+    }
+}
