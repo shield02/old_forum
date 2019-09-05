68f6847a089315698490d65118bfacbb508c3dec
shield02 <myshield02@gmail.com>
2019-09-05 21:07:21 +0100

    refactor flash to use data property optionally

--- public/js/app.js
+++ public/js/app.js
@@ -3187,7 +3187,7 @@ __webpack_require__.r(__webpack_exports__);
   props: ['message'],
   data: function data() {
     return {
-      body: '',
+      body: this.message,
       level: 'success',
       show: false
     };
@@ -3196,7 +3196,7 @@ __webpack_require__.r(__webpack_exports__);
     var _this = this;
 
     if (this.message) {
-      this.flash(this.message);
+      this.flash();
     }
 
     window.events.$on('flash', function (data) {
@@ -3205,8 +3205,11 @@ __webpack_require__.r(__webpack_exports__);
   },
   methods: {
     flash: function flash(data) {
-      this.body = data.message;
-      this.level = data.level;
+      if (data) {
+        this.body = data.message;
+        this.level = data.level;
+      }
+
       this.show = true;
       this.hide();
     },
--- resources/js/components/FlashComponent.vue
+++ resources/js/components/FlashComponent.vue
@@ -15,7 +15,7 @@
 
         data() {
             return {
-                body: '',
+                body: this.message,
                 level: 'success',
                 show: false
             }
@@ -23,7 +23,7 @@
 
         created() {
             if (this.message) {
-                this.flash(this.message);
+                this.flash();
             }
 
             window.events.$on('flash', data => this.flash(data));
@@ -31,8 +31,11 @@
 
         methods: {
             flash(data) {
-                this.body = data.message;
-                this.level = data.level;
+                if (data) {
+                    this.body = data.message;
+                    this.level = data.level;
+                }
+                
                 this.show = true;
 
                 this.hide();
