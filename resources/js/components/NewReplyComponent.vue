ef8119c009cc67e3678e14f39a2be34377742995
shield02 <myshield02@gmail.com>
2019-10-27 17:28:19 +0100

    minor modifications

--- resources/js/components/NewReplyComponent.vue
+++ resources/js/components/NewReplyComponent.vue
@@ -33,12 +33,6 @@
             }
         },
 
-        computed: {
-            signedIn() {
-                return window.App.signedIn;
-            }
-        },
-
         mounted() {
             $('#body').atwho({
                 at: "@",
